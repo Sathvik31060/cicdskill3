542f030aac5d64ab68d9d04d6e93b30e20574771
Sathvik31060 <163741485+Sathvik31060@users.noreply.github.com>
2025-07-22 16:59:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/s207">
+    <Router basename="/cicdskill3">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
